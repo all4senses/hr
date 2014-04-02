330abdefc614dea93a9e13551dce31bb3cb19828
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-04-02 16:31:49 +0700

    Home design

---
+++ themes/hr_aqua/templates/page.tpl.php
@@ -0,0 +1,176 @@
+<div id="c-top">
+
+    <header id="header" role="banner" class="clearfix">
+
+    <nav id="navigation" role="navigation" class="clearfix">
+      
+      
+      <div id="logo-block">
+        <a href="<?php print $front_page; ?>" id="logo">
+          <img src="<?php echo '/sites/all/themes/hr/css/images/hr-logo-main-top.png'; ?>" alt="HostingReview.org" title="<?php print 'HostingReview.org Home'; ?>" />
+        </a>
+        
+      </div>
+      
+      <?php 
+
+          
+
+//        if ($user->uid == 1) {
+          echo /*'<a id="itexpo" href="http://hostingreview.org/blog/tags/itexpo-2012"></a>',*/ render($page['header']); 
+          
+          echo hr_blocks_getBlockThemed(array('module' => 'om_maximenu', 'delta' => 'om-maximenu-1', 'no_subject' => TRUE, 'class' => 'block-om-maximenu', 'shadow' => FALSE), TRUE, '+31 day', ($user->uid ? '_logged' : NULL));
+          
+//        }
+//        else {
+//          echo '<div id="block-hr-blocks-header-links"><div class="follow-us">Follow Us</div>', hr_blocks_get_headerLinks(), '</div>', render($page['header']); 
+//        }
+      ?>
+    </nav> <!-- /#navigation -->
+
+    <?php ////if ($breadcrumb): print $breadcrumb; endif;?>
+  </header> <!-- /#header -->
+
+</div>
+
+
+
+
+    
+  <?php
+  /*
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+    <?php if ($main_menu): ?>
+      <a href="#navigation" class="element-invisible element-focusable"><?php print t('Skip to navigation'); ?></a>
+    <?php endif; ?>
+  </div>
+  */
+  ?>
+
+  
+
+  
+  <?php /* if ($page['highlighted']): ?>
+    <div id="highlighted-wrapper">
+      <section id="highlighted" class="clearfix">
+        <?php print render($page['highlighted']); ?>
+      </section>
+    </div>
+  <?php endif; */?>
+  
+  <div id="all-content-wrapper">
+  <div id="all-content" class="clearfix">
+      
+      
+      <?php if ($page['highlighted']): ?>
+          <?php 
+//            $url = 'http://hostingreview.org';
+//            $share_title = hr_misc_metatag_getFrontTitle();
+//            echo '<div class="float share">' . hr_blocks_getSocialiteButtons($url, $share_title) . '</div>';
+          ?>
+          <section id="highlighted" class="clearfix">
+            <?php print render($page['highlighted']); ?>
+          </section>
+      <?php endif; ?>
+      
+    
+      <section id="main" role="main" class="clearfix">
+
+          <?php 
+            /*
+            if ($breadcrumb): 
+              print $breadcrumb;
+            endif;
+            */
+            
+            // Hide breadcrumbs temporarily, but reserve the room for it.
+            //echo $breadcrumb; 
+          ?>
+          <div id="breadcrumbs-dummy"></div>
+        
+          <?php 
+          
+          //dpm($_SESSION['messages']);
+          //dpm($messages);
+          
+
+          print $messages; 
+            // we aren't getting messages, get them manually
+//            if (isset($_SESSION['messages'])) {
+//                echo '<div class="messages">';
+//                foreach($_SESSION['messages'] as $type=>$messages) {
+//                    echo "<p class=\"$type\">".implode("</p><p class=\"$type\">", $messages)."</p>";
+//                }
+//                echo '</div>';
+//                unset($_SESSION['messages']);
+//            }
+
+            
+          ?>
+          <a id="main-content"></a>
+          
+          <?php /* if (!$is_front && $title): ?>
+            <?php print render($title_prefix); ?>
+            <h1 class="title" id="page-title"><?php print $title; ?></h1>
+            <?php print render($title_suffix); ?>
+          <?php endif; */ ?>
+          
+          <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper clearfix"><?php print render($tabs); ?></div><?php endif; ?>
+          <?php print render($page['help']); ?>
+          <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+          
+          <?php print render($page['above_content']); ?>
+          <?php print render($page['content']); ?>
+          
+          
+      </section> <!-- /#main -->
+
+
+      <?php if ($page['sidebar_first']): ?>
+        <aside id="sidebar-first" role="complementary" class="sidebar clearfix">
+          <?php print render($page['sidebar_first']); ?>
+        </aside>  <!-- /#sidebar-first -->
+      <?php endif; ?>
+
+      <?php if ($page['sidebar_second']): ?>
+        <aside id="sidebar-second" role="complementary" class="sidebar clearfix">
+          <?php print render($page['sidebar_second']); ?>
+        </aside>  <!-- /#sidebar-second -->
+      <?php endif; ?>
+
+        
+  </div> <!-- /#all-content -->
+</div>
+  
+
+  
+  <footer id="footer" role="contentinfo" class="clearfix">
+    <div id="footer-inside">
+
+      <!--<div id="in-touch"></div> -->
+      
+      
+      
+      <?php 
+        
+        echo render($page['footer']);
+        
+        //echo hr_blocks_getBlockThemed(array('module' => 'hr_blocks', 'delta' => 'send_msg_n_subscribe', 'no_subject' => TRUE/*, 'class' => 'block-om-maximenu'*/, 'shadow' => FALSE)/*, TRUE, '+31 day'*/ /*, ($user->uid ? '_logged' : NULL)*/);
+        ////echo hr_blocks_getBlockThemed(array('module' => 'hr_blocks', 'delta' => 'social_links', 'no_subject' => TRUE/*, 'class' => 'block-om-maximenu'*/, 'shadow' => FALSE)/*, TRUE, '+31 day'*/ /*, ($user->uid ? '_logged' : NULL)*/);
+        //echo hr_blocks_getBlockThemed(array('module' => 'hr_blocks', 'delta' => 'footer_menu', 'no_subject' => TRUE/*, 'class' => 'block-om-maximenu'*/, 'shadow' => FALSE)/*, TRUE, '+31 day'*/ /*, ($user->uid ? '_logged' : NULL)*/);
+        
+        echo hr_blocks_get_footerLinks() . '<div class="c">© 2014 hostingreview.org | All Rights Reserved</div>';
+        
+                
+        //echo '<div id="block-hr-blocks-follow-links"><div class="follow-us">Follow Us</div>', hr_blocks_get_headerLinks(), '</div>';
+      ?>
+      
+      
+    </div>
+  </footer> <!-- /#footer -->
+  <?php /* 
+  <div id="under-footer">
+    <div class="c">Copyright © 2014 Cloud Hosting HQ | All rights reserved.</div>
+  </div>
+  */ ?>

c60a07a49318d30434c978727de041880d105bc3
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-04-03 09:44:11 +0700

    Reviews rotator

--- modules/_HR/hr_blocks/js/hr_reviewsRotator.js
+++ modules/_HR/hr_blocks/js/hr_reviewsRotator.js
@@ -6,6 +6,8 @@
       //$("#r-rotator > ul").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true); // for a Version of UI > 1.9
       var rtabs = $("#r-rotator").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 3000, true);
        console.log(rtabs);
+       var ltabs = rtabs.tabs('length');
+       console.log('len: ' + ltabs);
        
       // Pause on hover.
       $("#r-rotator").hover(  

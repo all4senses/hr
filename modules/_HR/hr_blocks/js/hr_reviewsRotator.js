35cb97e91d63714e8165a6bdf33cc21714970217
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-04-03 09:35:08 +0700

    Reviews rotator

--- modules/_HR/hr_blocks/js/hr_reviewsRotator.js
+++ modules/_HR/hr_blocks/js/hr_reviewsRotator.js
@@ -4,8 +4,8 @@
     attach: function (context, settings) {
        
       //$("#r-rotator > ul").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true); // for a Version of UI > 1.9
-      var tabs = $("#r-rotator").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 3000, true);
-       console.log(tabs);
+      var rtabs = $("#r-rotator").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 3000, true);
+       console.log(rtabs);
        
       // Pause on hover.
       $("#r-rotator").hover(  
@@ -23,6 +23,7 @@
           console.log('click');
           var active = $( "#r-rotator" ).tabs( "option", "active" );
           console.log(active);
+          rtabs.tabs('select', 2);
           //$( "#r-rotator" ).tabs( "option", "active", active + 1 );
 
       });

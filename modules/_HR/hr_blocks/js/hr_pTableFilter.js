df509275199054ee225e2b56a77dc88314af7855
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-05-31 05:08:18 +0700

    Table sort

--- modules/_HR/hr_blocks/js/hr_pTableFilter.js
+++ modules/_HR/hr_blocks/js/hr_pTableFilter.js
@@ -4,12 +4,12 @@
     attach: function (context, settings) {
         
         var data = 0;
-        console.log('Initiate filter...');
+        //console.log('Initiate filter...');
         
         
         $("#p_filter").change(function() {
-          console.log('On change...');
-          console.log($(this).val());
+          //console.log('On change...');
+          //console.log($(this).val());
           // Works
           //alert( this.value ); // or $(this).val()
           
@@ -21,11 +21,11 @@
           switch ($(this).val()) {
             
               case "All": 
-                console.log("Show All"); 
+                //console.log("Show All"); 
                 
                 $( ".view-display-id-block_top_wp_p_table tbody tr, .view-display-id-block_h_top_sh_table tbody tr" ).each(function( index ) {
 
-                    console.log('index #' + index);
+                    //console.log('index #' + index);
                     //console.log(this);
                     $(this).removeClass('hidden');
                     
@@ -33,21 +33,23 @@
                 break;
                 
                 
+                
+                
               case "Top 5": 
-                console.log("a1"); 
+                //console.log("a1"); 
                 
                 $( ".view-display-id-block_top_wp_p_table tbody tr, .view-display-id-block_h_top_sh_table tbody tr" ).each(function( index ) {
                   
-                  console.log('index #' + index);
+                  //console.log('index #' + index);
                   if (index > 4) 
                   {
                     
                     $(this).addClass('hidden');
-                    console.log('Added from ' + index);
+                    //console.log('Added from ' + index);
                   }
                   else {
                     $(this).removeClass('hidden');
-                    console.log('Removed from ' + index);
+                    //console.log('Removed from ' + index);
                   }
   
                 });
@@ -55,16 +57,20 @@
                 
                 break;
                 
+                
+                
+                
+                
               case "Best Refund": 
-                console.log("a2"); 
+                //console.log("a2"); 
                 
                 $( ".view-display-id-block_top_wp_p_table tbody tr, .view-display-id-block_h_top_sh_table tbody tr" ).each(function( index ) {
 
-                    console.log('index #' + index);
+                    ////console.log('index #' + index);
                     //console.log(this);
                     
                     data = $(this).find("td.views-field-php-6").html().trim();
-                    console.log('Refund = <' + data + '>');
+                    //console.log('Refund = <' + data + '>');
                     if (data && data == 'Anytime') {
                       $(this).removeClass('hidden');
                     }
@@ -79,15 +85,15 @@
               
               
               case "Worst Refund": 
-                console.log("a3"); 
+                //console.log("a3"); 
                 
                 $( ".view-display-id-block_top_wp_p_table tbody tr, .view-display-id-block_h_top_sh_table tbody tr" ).each(function( index ) {
 
-                    console.log('index #' + index);
+                    ////console.log('index #' + index);
                     //console.log(this);
                     
                     data = $(this).find("td.views-field-php-6").html().trim();
-                    console.log('Refund = <' + data + '>');
+                    //console.log('Refund = <' + data + '>');
                     if (data && data == '30 Days') {
                       $(this).removeClass('hidden');
                     }
@@ -101,12 +107,12 @@
                 
               
               case "Best Under $3": 
-                console.log("a4"); 
+                //console.log("a4"); 
                 
                 $( ".view-display-id-block_top_wp_p_table tbody tr, .view-display-id-block_h_top_sh_table tbody tr" ).each(function( index ) {
                   //if (index) 
                   {
-                    console.log('index #' + index);
+                    //console.log('index #' + index);
                     //console.log(this);
                     
                     data = $(this).find("td.views-field-php-2").html().trim();
@@ -116,14 +122,14 @@
                       data = data.replace('<div class="price month non-crossed">','');
                       data = data.replace('</div>','');
                       
-                      console.log('data = ' + data);
+                      //console.log('data = ' + data);
 
                       if (data < 3) {
-                        console.log(data + ' is less than 3');
+                        //console.log(data + ' is less than 3');
                         $(this).removeClass('hidden');
                       }
                       else {
-                        console.log(data + ' is more than 3');
+                        //console.log(data + ' is more than 3');
                         $(this).addClass('hidden');
                       }
                     }
@@ -134,11 +140,11 @@
                 
                 
               case "Best Under $4": 
-                console.log("a5"); 
+                //console.log("a5"); 
                 
                 $( ".view-display-id-block_top_wp_p_table tbody tr, .view-display-id-block_h_top_sh_table tbody tr" ).each(function( index ) {
                   if (index) {
-                    console.log('index #' + index);
+                    //console.log('index #' + index);
                     //console.log(this);
                     
                     data = $(this).find("td.views-field-php-2").html().trim();
@@ -148,14 +154,14 @@
                       data = data.replace('<div class="price month non-crossed">','');
                       data = data.replace('</div>','');
                       
-                      console.log('data = ' + data);
+                      //console.log('data = ' + data);
 
                       if (data < 4) {
-                        console.log(data + ' is less than 4');
+                        //console.log(data + ' is less than 4');
                         $(this).removeClass('hidden');
                       }
                       else {
-                        console.log(data + ' is more than 4');
+                        //console.log(data + ' is more than 4');
                         $(this).addClass('hidden');
                       }
                     }

ea86c7679aa11a773585dcdefbdcfd28b4e11327
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-05-31 00:52:42 +0700

    Table sort

--- modules/_HR/hr_blocks/js/hr_pTableFilter.js
+++ modules/_HR/hr_blocks/js/hr_pTableFilter.js
@@ -22,12 +22,12 @@
               case "Top 5": console.log("a1"); break;
               case "Best Refund": console.log("a2"); break;
               case "Worst Refund": console.log("a3"); break;
-              default: 
-                //console.log("default"); 
+              
+              case "Best Under $3": 
+                console.log("a4"); 
                 
                 $( ".view-id-providers tbody tr" ).each(function( index ) {
-                  //if (index == 3) 
-                  {
+                  if (index) {
                     console.log('index #' . index);
                     //console.log(this);
                     
@@ -35,16 +35,45 @@
                     
                     console.log('price = ' + price);
                     
-                    if (price > 3) {
-                      console.log(price + ' is more than 3');
+                    if (price < 3) {
+                      console.log(price + ' is less than 3');
                     }
                     else {
-                      console.log(price + ' is less than 3');
+                      console.log(price + ' is more than 3');
                     }
                   }
                 });
-                //
                 
+                break;
+                
+                
+              case "Best Under $4": 
+                console.log("a5"); 
+                
+                $( ".view-id-providers tbody tr" ).each(function( index ) {
+                  if (index) {
+                    console.log('index #' . index);
+                    //console.log(this);
+                    
+                    price = $(this).find("td.views-field-php-2").html().replace('$','');
+                    
+                    console.log('price = ' + price);
+                    
+                    if (price < 4) {
+                      console.log(price + ' is less than 4');
+                    }
+                    else {
+                      console.log(price + ' is more than 4');
+                    }
+                  }
+                });
+                
+                break;
+                
+                
+              
+              default: 
+                console.log("default"); 
                 break;;
           }
         

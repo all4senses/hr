4caedf78f7ca2661fc388c1daadce78272f80980
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-09-17 02:31:32 +0700

    Home chart rotated providrs

---
+++ modules/_HR/hr_blocks/js/hr_rotate_cached_page_table.js
@@ -0,0 +1,14 @@
+(function ($) {
+
+  Drupal.behaviors.hr_rotate_cached_page_table = {
+    attach: function (context, settings) {
+
+        
+        console.log('hr_rotate_cached_page_table...');
+        console.log(Drupal.settings['hr_blocks']['hr_current_top_positions_counter']); 
+        
+        
+    }
+  };
+
+}(jQuery));
